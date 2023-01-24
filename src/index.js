531026d91716f068b9531175313319036c7c951b
Isabella Persaud <isabellapersaud041600@gmail.com>
2023-01-23 22:49:49 -0500

    update

--- src/index.js
+++ src/index.js
@@ -11,3 +11,27 @@ exitButton.addEventListener('click', () => {
     nav.classList.remove('menu-btn')
 })
 
+
+const cardList=document.querySelector('.card')
+function renderDomestic(cards) {
+    cards.forEach((card) => {
+        const domesticCard = document.createElement('div')
+        domesticCard.className = 'card'
+
+        const location= document.querySelector("#location")
+        location.textContent= card.location;
+
+        const img = document.querySelector("body > div.card > img")
+        img.className = 'img'
+
+
+        const activity = document.querySelector("#activity")
+        activity.textContent = card.activity
+
+        const hotel = document.querySelector("#hotel")
+        hotel.textContent = card.hotel
+
+        const restaurant = document.querySelector("#restaurant")
+        restaurant.textContent = card.restaurant
+
+})
